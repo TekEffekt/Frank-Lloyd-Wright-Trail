9416104e1c4617d301c120ad2a586e79d1fed7a2
Kyle Zawacki <z95sk8@outlook.com>
2016-08-03 16:32:42 -0500

    Initial Commit

---
+++ Pods/Target Support Files/Pods-Frank Lloyd Wright Trail/Pods-Frank Lloyd Wright Trail-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Frank_Lloyd_Wright_Trail : NSObject
+@end
+@implementation PodsDummy_Pods_Frank_Lloyd_Wright_Trail
+@end
---
+++ Pods/Target Support Files/Pods-Frank Lloyd Wright Trail/Pods-Frank Lloyd Wright Trail-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Frank_Lloyd_Wright_TrailVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Frank_Lloyd_Wright_TrailVersionString[];
+
