8a0a35f48d81a0c6a4633cc91e4fd4e2925bbfbd
Mikial <horni005@rangers.uwp.edu>
2017-02-22 17:26:23 -0600

    ready to pull from tripodal

---
+++ Frank Lloyd Wright Trail/Date+Comparable.swift
@@ -0,0 +1,51 @@
+//
+//  Date+Comparable.swift
+//  Frank Lloyd Wright Trail
+//
+//  Created by Max on 1/31/17.
+//  Copyright © 2017 App Factory. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension NSDate {
+    func isGreaterThanDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isGreater = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedDescending {
+            isGreater = true
+        }
+        
+        //Return Result
+        return isGreater
+    }
+    
+    func isLessThanDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isLess = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedAscending {
+            isLess = true
+        }
+        
+        //Return Result
+        return isLess
+    }
+    
+    func equalToDate(dateToCompare: NSDate) -> Bool {
+        //Declare Variables
+        var isEqualTo = false
+        
+        //Compare Values
+        if self.compare(dateToCompare) == NSComparisonResult.OrderedSame {
+            isEqualTo = true
+        }
+        
+        //Return Result
+        return isEqualTo
+   }
+}
