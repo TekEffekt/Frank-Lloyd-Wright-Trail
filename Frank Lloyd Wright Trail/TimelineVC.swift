af9ef469625213a0bc99ea56135d9f78be75f1db
Mikial <horni005@rangers.uwp.edu>
2017-02-22 18:02:13 -0600

    added gitignore

--- Frank Lloyd Wright Trail/TimelineVC.swift
+++ Frank Lloyd Wright Trail/TimelineVC.swift
@@ -28,15 +28,7 @@ class TimelineVC: UIViewController {
             NSLayoutConstraint(item: scrollView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: 0)
             ])
         
-        timeline = TimelineView(bulletType: .Circle, timeFrames: [
-            TimeFrame(text: "New Year's Day", date: "January 1", image: UIImage(named: "meetinghouse")),
-            TimeFrame(text: "The month of love!", date: "February 14", image: UIImage(named: "meetinghouse")),
-            TimeFrame(text: "Comes like a lion, leaves like a lamb", date: "March",  image: nil),
-            TimeFrame(text: "Dumb stupid pranks.", date: "April 1", image: UIImage(named: "meetinghouse")),
-            TimeFrame(text: "That's right. No image is necessary!", date: "No image?", image: nil),
-            TimeFrame(text: "This control can stretch. It doesn't matter how long or short the text is, or how many times you wiggle your nose and make a wish. The control always fits the content, and even extends a while at the end so the scroll view it is put into, even when pulled pretty far down, does not show the end of the scroll view.", date: "Long text", image: nil),
-            TimeFrame(text: "Hope this helps someone!", date: "That's it!", image: nil)
-            ])
+        timeline = TimelineView(bulletType: .Circle, timeFrames: [])
         scrollView.addSubview(timeline)
         scrollView.addConstraints([
             NSLayoutConstraint(item: timeline, attribute: .Left, relatedBy: .Equal, toItem: scrollView, attribute: .Left, multiplier: 1.0, constant: 0),
