5ff11bdb945bcb38aad35f180f8e5270945943af
Mikial <horni005@rangers.uwp.edu>
2017-02-27 15:26:10 -0600

    fixed issue where some of the sites weren't being created in the time line view

--- Frank Lloyd Wright Trail/TimelineVC.swift
+++ Frank Lloyd Wright Trail/TimelineVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TimelineVC: UIViewController, TripJsonDelegate {
-
+    
     
     var scrollView: UIScrollView!
     var timeline:   TimelineView!
@@ -34,40 +34,40 @@ class TimelineVC: UIViewController, TripJsonDelegate {
         json = JsonParser(withDelegate: self, locations: sites)
         getTripData(json.orderOfLocations(sites))
     }
-
-
+    
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-
+    
     func getTripData(objects: [TripObject]) {
         var timeFrames: [TimeFrame] = []
         
         for i in 0..<objects.count{
             for j in 0..<sites.count{
-              Double(round(1000*objects[i].endPoint!)/100)
-              Double(round(1000*sitesImage[j].lat)/100)
-                if(Double(round(1000*objects[i].endPoint!)/1000) == Double(round(1000*sitesImage[j].lat)/1000)){
+                Double(round(100*objects[i].endPoint!)/100)
+                Double(round(100*sitesImage[j].lat)/100)
+                if(Double(round(100*objects[i].endPoint!)/100) == Double(round(100*sitesImage[j].lat)/100)){
                     timeFrames.append(TimeFrame(text:"Travel distance is " + objects[i].distanceText! + " Travel time is " + objects[i].timeText!, date: sitesImage[j].title, image: UIImage(named:sitesImage[j].imageName!)))
+                }
             }
-            }
-           
+            
         }
         
-            timeline = TimelineView(bulletType: .Circle, timeFrames: timeFrames)
+        timeline = TimelineView(bulletType: .Circle, timeFrames: timeFrames)
+        
+        scrollView.addSubview(timeline)
+        scrollView.addConstraints([
+            NSLayoutConstraint(item: timeline, attribute: .Left, relatedBy: .Equal, toItem: scrollView, attribute: .Left, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: timeline, attribute: .Bottom, relatedBy: .LessThanOrEqual, toItem: scrollView, attribute: .Bottom, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: timeline, attribute: .Top, relatedBy: .Equal, toItem: scrollView, attribute: .Top, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: timeline, attribute: .Right, relatedBy: .Equal, toItem: scrollView, attribute: .Right, multiplier: 1.0, constant: 0),
             
-            scrollView.addSubview(timeline)
-            scrollView.addConstraints([
-                NSLayoutConstraint(item: timeline, attribute: .Left, relatedBy: .Equal, toItem: scrollView, attribute: .Left, multiplier: 1.0, constant: 0),
-                NSLayoutConstraint(item: timeline, attribute: .Bottom, relatedBy: .LessThanOrEqual, toItem: scrollView, attribute: .Bottom, multiplier: 1.0, constant: 0),
-                NSLayoutConstraint(item: timeline, attribute: .Top, relatedBy: .Equal, toItem: scrollView, attribute: .Top, multiplier: 1.0, constant: 0),
-                NSLayoutConstraint(item: timeline, attribute: .Right, relatedBy: .Equal, toItem: scrollView, attribute: .Right, multiplier: 1.0, constant: 0),
-                
-                NSLayoutConstraint(item: timeline, attribute: .Width, relatedBy: .Equal, toItem: scrollView, attribute: .Width, multiplier: 1.0, constant: 0)
-                ])
-            
-            view.sendSubviewToBack(scrollView)
-        }
+            NSLayoutConstraint(item: timeline, attribute: .Width, relatedBy: .Equal, toItem: scrollView, attribute: .Width, multiplier: 1.0, constant: 0)
+            ])
+        
+        view.sendSubviewToBack(scrollView)
+    }
     
-
+    
 }
