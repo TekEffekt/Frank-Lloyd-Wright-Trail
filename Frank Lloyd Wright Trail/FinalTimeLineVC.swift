402fd7f43a98e6da5cde1f708096b6c07742303c
Max <roger048@rangers.uwp.edu>
2017-05-01 12:49:24 -0500

    Constraint updates

--- Frank Lloyd Wright Trail/FinalTimeLineVC.swift
+++ Frank Lloyd Wright Trail/FinalTimeLineVC.swift
@@ -29,7 +29,7 @@ class FinalTimeLineVC: UIViewController {
         
         view.addConstraints([
             NSLayoutConstraint(item: scrollView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0),
-            NSLayoutConstraint(item: scrollView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 29),
+            NSLayoutConstraint(item: scrollView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0),
             NSLayoutConstraint(item: scrollView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 0),
             NSLayoutConstraint(item: scrollView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0)
             ])
